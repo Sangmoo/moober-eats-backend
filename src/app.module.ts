2548368d03431d32cd9730de30d6c1fb674f2d37
Sangmoo <first_1st@naver.com>
2020-10-18 01:50:35 +0900

    Resolver Decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot()
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from "@nestjs/graphql";
+
+
+
+
+@Resolver()
+export class RestaurantResolver {
+    
+    @Query(returns => Boolean)
+    isPizzaGood(): Boolean {
+        return true;
+    }
+}
