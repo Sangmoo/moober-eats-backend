9d21c7d6df27eec01b0a5bd69aeee41a12a7834d
Sangmoo <first_1st@naver.com>
2020-10-19 00:54:32 +0900

    Gql Arg

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 // Use Restaurant Entity
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(returns => [Restaurant])
+  restaurant(@Args('vegan') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
