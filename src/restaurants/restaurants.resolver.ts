4b26502ff752bf411db69c8c88b14e324e46dbac
Sangmoo <first_1st@naver.com>
2020-10-18 11:47:31 +0900

    Fix to Prettier Set

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,14 +1,11 @@
-import { Query, Resolver } from "@nestjs/graphql";
-import { Restaurant } from "./entities/restaurant.entity";
-
-
+import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
 // Use Restaurant Entity
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
-    
-    @Query(returns => Restaurant)
-    myRestaurant() {
-        return true;
-    }
+  @Query(returns => Restaurant)
+  myRestaurant() {
+    return true;
+  }
 }
