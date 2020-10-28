b221d6bf6f70ad4094ad65ff35f498d022ac0cb0
Sangmoo <first_1st@naver.com>
2020-10-29 01:00:59 +0900

    Create Entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { type } from 'os';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
   @Field(type => String)
+  @Column()
   name: string;
 
   @Field(type => Boolean, { nullable: true })
+  @Column()
   isGood?: boolean;
 
   @Field(type => String)
+  @Column()
   address: string;
 
   @Field(type => String)
+  @Column()
   ownerName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
 }
