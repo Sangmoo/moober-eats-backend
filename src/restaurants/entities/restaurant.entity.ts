f9a2719d97c14e6a89b49e26e741a09a3495587e
Sangmoo <first_1st@naver.com>
2020-11-23 00:20:17 +0900

    Mapped Types - Omit, Input

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,8 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -10,11 +12,14 @@ export class Restaurant {
 
   @Field(type => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field(type => Boolean, { nullable: true })
   @Column()
-  isGood?: boolean;
+  @IsBoolean()
+  isVegan?: boolean;
 
   @Field(type => String)
   @Column()
