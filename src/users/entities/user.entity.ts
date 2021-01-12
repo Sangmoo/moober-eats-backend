79675108afa4ded430f7a347b85c255c32e36725
Sangmoo <first_1st@naver.com>
2021-01-12 23:24:37 +0900

    Create VerificationEntity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -38,6 +38,10 @@ export class User extends CoreEntity {
   @IsEnum(UserRole)
   role: UserRole;
 
+  @Column({ default: false })
+  @Field(type => Boolean)
+  verified: boolean;
+
   @BeforeInsert()
   @BeforeUpdate()
   async hashPassword(): Promise<void> {
