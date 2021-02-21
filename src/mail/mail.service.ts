3713c97781e76d117901aed721689cb5d79215e4
Sangmoo <first_1st@naver.com>
2021-02-22 00:12:53 +0900

    Sent Mail Test

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,43 @@
+import got from 'got';
+import * as FormData from 'form-data';
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions, // private readonly configService: ConfigService,
+  ) {
+    // Check Account Setting Error Handling
+    this.sendEmail('testing', 'testContent')
+      .then(() => {
+        console.log('Message sent');
+      })
+      .catch(error => {
+        console.log(error.response.body);
+      });
+  }
+
+  // After Add toEmail
+  private async sendEmail(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `first_1st@naver.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
+}
